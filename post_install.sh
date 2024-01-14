212a95603bf58c776eac36ff6eefa7535c5b723b
c0rvax <C0rvax@pm.me>
2024-01-14 15:07:35 +0100

    mise à jour 14-01-24

--- post_install.sh
+++ post_install.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-# Mandatory
+	# UPDATE
 
 sudo apt-get update && sudo apt-get upgrade
+
+	# FUNCTIONS
 
 function get_package {
 	sudo apt-get install -y ${1}
 }
+
+	# LIST OF PACKAGES
+
 PKGS=(
 
 	# PACMAN
@@ -16,6 +21,11 @@ PKGS=(
 	'clang'
 	'gcc'
 	'make'
+	# UTILS
+	'gimp'
+	'htop'
+	'neofetch'
+	'ufw'
 	# TERM
 	'terminator'
 	# NVIM DEPEDENCIES
@@ -32,3 +42,20 @@ PKGS=(
 for PKG in "${PKGS[@]}"; do
 	get_package ${PKG}
 done
+
+	# INSTALL NVIM + CONFIG
+
+cd $HOME
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+git clone git@github.com:C0rvax/nvim.git $HOME/.config/nvim
+
+	# INSTALL VERACRYPT
+
+sudo add-apt-repository ppa:unit193/encryption -y
+sudo apt-get update -y
+sudo apt-get install veracrypt -y
+
+	# CLEANING
+
+sudo apt-get autoclean -y && sudo apt-get autoremove -y
