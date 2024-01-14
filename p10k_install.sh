2d78f3df466677ac61c733004bbdf6e7db8ac385
c0rvax <C0rvax@pm.me>
2024-01-14 23:42:29 +0100

    marche mais tjrs pas de verif et mettre les prefs de git

--- p10k_install.sh
+++
@@ -1,4 +0,0 @@
- 
-cd ..
-
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
--- post_install.sh
+++ post_install.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
-	# UPDATE
+# Code couleur
+rouge='\e[1;31m'
+jaune='\e[1;33m'
+bleu='\e[1;34m'
+violet='\e[1;35m'
+vert='\e[1;32m'
+neutre='\e[0;m'
+
+# Vérification que le script n'est pas lancé directement avec sudo (le script contient déjà les sudos pour les actions lorsque c'est nécessaire)
+if [ "$UID" -eq "0" ]; then
+	echo -e "${rouge}Merci de ne pas lancer directement ce script avec les droits root : lancez le sans sudo , le mot de passe sera demandé dans le terminal lors de la 1ère action nécessitant le droit administrateur.${neutre}"
+	exit
+fi
+
+# UPDATE
 
 sudo apt-get update && sudo apt-get upgrade
 
-	# FUNCTIONS
+# FUNCTIONS
 
 function get_package {
 	sudo apt-get install -y ${1}
 }
 
-	# LIST OF PACKAGES
+# LIST OF PACKAGES
 
 PKGS=(
 
@@ -43,19 +57,24 @@ for PKG in "${PKGS[@]}"; do
 	get_package ${PKG}
 done
 
-	# INSTALL NVIM + CONFIG
+# INSTALL NVIM + CONFIG
 
 cd $HOME
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 git clone git@github.com:C0rvax/nvim.git $HOME/.config/nvim
 
-	# INSTALL VERACRYPT
+# INSTALL VERACRYPT
 
 sudo add-apt-repository ppa:unit193/encryption -y
 sudo apt-get update -y
 sudo apt-get install veracrypt -y
 
-	# CLEANING
+# CLEANING
 
 sudo apt-get autoclean -y && sudo apt-get autoremove -y
+
+# INSTALL OH MY ZSH
+
+cd $HOME
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
