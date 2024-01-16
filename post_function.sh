bd17ab6eac9f940b1c4eec374d1c63934d9f1189
c0rvax <C0rvax@pm.me>
2024-01-16 14:56:57 +0100

    marche bien

--- post_function.sh
+++ post_function.sh
@@ -1,26 +1,23 @@
 # FUNCTIONS
 
-function check_file
-{
-	if [ -f "${1}" ];then
+function check_file {
+	if [ -f "${1}" ]; then
 		return 1
 	else
 		return 0
 	fi
 }
 
-function check_directory
-{
-	if [ -d "${1}" ];then
+function check_directory {
+	if [ -d "${1}" ]; then
 		return 1
 	else
 		return 0
 	fi
 }
 
-function check_package
-{
-	dpkg -s ${1} &> /dev/null
+function check_package {
+	dpkg -s ${1} &>/dev/null
 
 	if [ $? -eq 0 ]; then
 		return 1
@@ -29,8 +26,7 @@ function check_package
 	fi
 }
 
-function install_package
-{
+function install_package {
 	check_package ${1}
 	if [ "$?" -eq "1" ]; then
 		echo -e "${vert} #### Package ${1} is installed! ####"
@@ -42,31 +38,28 @@ function install_package
 
 function get_package {
 
-sudo apt-get install -y ${1}
+	sudo apt-get install -y ${1}
 
 }
 
 # Check if launched with sudo
-function check_sudo
-{
-if [ "$UID" -eq "0" ]; then
-	echo -e "${rouge}Do not execute with sudo, Your password will be asked in the console.${neutre}"
-	exit
-fi
+function check_sudo {
+	if [ "$UID" -eq "0" ]; then
+		echo -e "${rouge}Do not execute with sudo, Your password will be asked in the console.${neutre}"
+		exit
+	fi
 }
 
 # Update
-function p_update
-{
-echo -e "${bleu}"
-sudo apt-get update && sudo apt-get upgrade
-echo -e "${neutre}"
+function p_update {
+	echo -e "${bleu}"
+	sudo apt-get update -y && sudo apt-get upgrade -y
+	echo -e "${neutre}"
 }
 
 # Clean
-function p_clean
-{
-echo -e "${bleu}"
-sudo apt-get autoclean -y && sudo apt-get autoremove -y
-echo -e "${neutre}"
+function p_clean {
+	echo -e "${bleu}"
+	sudo apt-get autoclean -y && sudo apt-get autoremove -y
+	echo -e "${neutre}"
 }
